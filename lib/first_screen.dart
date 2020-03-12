ecc62014e12df610e1e086b9a5091e7b689b5ea9
thembeka <thembeka.george@meltwater.org>
2020-03-12 12:17:45 +0000

    ...

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -11,9 +11,32 @@ class FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        body: Container(
+         child: Image(
+        image: AssetImage('assets/logo.png'),
+      ),
+    ));
   }
+}
+
+
+class Home extends StatefulWidget {
   @override
+  _HomeState createState() => _HomeState();
+}
 
-
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Image.asset('assets/logo.jpg'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[600],
+        child: Text('click'),
+      ),
+    );
+  }
 }
