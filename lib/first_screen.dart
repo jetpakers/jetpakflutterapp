322592ab66aedd46d4161b2be532082339801ab1
thembeka <thembeka.george@meltwater.org>
2020-03-12 15:09:05 +0000

    rounded buttons

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatefulWidget {
@@ -20,19 +21,31 @@ class FirstScreenState extends State<FirstScreen> {
             child: Image(image: AssetImage('assets/images/jetPakhome.png')),
           ),
           RaisedButton(
-            color: Colors.deepOrange,
-            onPressed: () {  },
-            child: Text ('Fly Jetpak',style: TextStyle (color: Colors.white),),
-    ),
+            color: Color(0xFFEA7313),
+            shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(12.0),
+                side: BorderSide(color: Colors.blueGrey)),
+            onPressed: () {},
+            child: Text(
+              'Fly Jetpak',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
           Column(
             children: <Widget>[
               RaisedButton(
                 color: Colors.white,
-                onPressed: () {  },
-                child: Text ('Send Jetpak',style: TextStyle (color: Colors.deepOrange),),
+                shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(12.0),
+                    side: BorderSide(color: Color(0xFF4E37B2))),
+                onPressed: () {},
+                child: Text(
+                  'Send Jetpak',
+                  style: TextStyle(color: Color(0xFF4E37B2)),
+                ),
               ),
             ],
-    ),
+          ),
         ],
       ),
     );
