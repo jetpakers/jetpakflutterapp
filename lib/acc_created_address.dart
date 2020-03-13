568172d3f1ea2407894cfbe62e8ba5ef56543356
thembeka <thembeka.george@meltwater.org>
2020-03-13 00:06:14 +0000

    added space between components,,

--- lib/acc_created_address.dart
+++ lib/acc_created_address.dart
@@ -17,36 +17,50 @@ class FourthScreenState extends State<FourthScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Spacer(flex: 1),
-          Container(
-              child: Text(
-            'Account created successfully !',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-          )),
+          Center(
+            child: Container(
+                child: Text(
+              'Account created successfully !',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            )),
+          ),
           Spacer(flex: 1),
           Container(
+            width: 300.0,
+            height: 200.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+//                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/acc_created.png')
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
             color: Color(0xFF4E37B2),
-            child: Center(
-              child: Image(image: AssetImage('assets/images/acc_created.png')),
             ),
           ),
           Spacer(),
           Container(
               child: Text(
             "Here's the Jetpak warehouse address",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
           )),
           Spacer(),
           Container(
               child: Text(
             "This is where you will send your goods.\nWe will Jetpak it to your destination.",
-            style: TextStyle(fontSize: 18),
+            style: TextStyle(fontSize: 20),
           )),
           Spacer(),
           Container(
-            color: Color(0xFF8574CE),
+            width: 300.0,
+            height: 150.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              color: Color(0xFF8574CE),
+            ),
             child: Text(
-                "(209)-543-6857 \n 4520 Sun Down PL \n Salida, California(CA), \n 95368",
-                style: TextStyle(fontSize: 22),
+              "\n (209)-543-6857 \n 4520 Sun Down PL \n Salida, California(CA), \n 95368",
+              style: TextStyle(fontSize: 22, color: Colors.white),
+              textAlign: TextAlign.center,
             ),
           ),
           Spacer(),
