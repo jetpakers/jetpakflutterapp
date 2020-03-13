5a629b660d42473ef403caf549c541c7019e79bd
thembeka <thembeka.george@meltwater.org>
2020-03-13 03:05:53 +0000

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:jetpaksender/order_invoice.dart';
 import 'package:jetpaksender/send_request_form.dart';
 import 'package:jetpaksender/first_screen.dart';
 import 'package:jetpaksender/create_app_account.dart';
 import 'package:jetpaksender/create_sender_account.dart';
 import 'package:jetpaksender/acc_created_address.dart';
 import 'package:jetpaksender/warehouse_address.dart';
+import 'package:jetpaksender/order_invoice.dart';
+
 
 void main() => runApp(JetpakApp());
 
---
+++ lib/order_invoice.dart
@@ -0,0 +1,297 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class InvoiceScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return InvoiceScreenState();
+  }
+}
+
+class InvoiceScreenState extends State<InvoiceScreen> {
+  String shipmentNumber;
+  String productType;
+  String productName;
+  String productDescription;
+  String chargeableWeight;
+  Double fees;
+ Double insuranceFee;
+ Double amount;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Widget _buildShipmentNumber() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: 'Shipment Number',
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(color: Colors.black))),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Shipment Number is Required';
+          }
+
+          return null;
+        },
+        onSaved: (String value) {
+          shipmentNumber = value;
+        },
+      ),
+    );
+  }
+
+  Widget _buildProductType() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: 'Product Type',
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(color: Colors.black))),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Product type is Required';
+          }
+
+          return null;
+        },
+        onSaved: (String value) {
+          productType = value;
+        },
+      ),
+    );
+  }
+
+  Widget _buildProductName() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: 'Product Name',
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(color: Colors.black))),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Product Name is Required';
+          }
+          return null;
+        },
+        onSaved: (String value) {
+          productName = value;
+        },
+      ),
+    );
+  }
+
+  Widget _buildDescription() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: 'Product Description',
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(color: Colors.black))),
+//      keyboardType: TextInputType.visiblePassword,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Product Description is Required';
+          }
+
+          return null;
+        },
+        onSaved: (String value) {
+          productDescription = value;
+        },
+      ),
+    );
+  }
+
+  Widget _buildChargeableWeight() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: 'Chargeable Weight',
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(color: Colors.black))),
+        keyboardType: TextInputType.number,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Chargeable Weight is Required';
+          }
+          return null;
+        },
+        onSaved: (String value) {
+          chargeableWeight = value;
+        },
+      ),
+    );
+  }
+
+  Widget _buildFees() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: 'Handling Fees and Taxes ',
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(color: Colors.black))),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Handling Fees and Taxes  is Required';
+          }
+
+          return null;
+        },
+        onSaved: (String value) {
+          fees = value as Double;
+        },
+      ),
+    );
+  }
+
+  Widget _buildInsuranceFees() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: 'Insurance Fees',
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(color: Colors.black))),
+        keyboardType: TextInputType.number,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Insurance Fees is Required';
+          }
+          return null;
+        },
+        onSaved: (String value) {
+          insuranceFee = value as Double;
+        },
+      ),
+    );
+  }
+
+  Widget _buildAmount() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: 'Amount',
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(color: Colors.black))),
+        keyboardType: TextInputType.number,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Amount is Required';
+          }
+          return null;
+        },
+        onSaved: (String value) {
+          amount = value as Double;
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 100),
+        child: Container(
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(color: Colors.black12, spreadRadius: 5, blurRadius: 2)
+          ]),
+          //width: MediaQuery.of(context).size.width,
+          height: 100,
+          child: Container(
+            decoration: BoxDecoration(
+                color: Color(0xFF4E37B2),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30))),
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.navigate_before,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "Order Invoice",
+                    style: TextStyle(fontSize: 25, color: Colors.white),
+                  ),
+                  Icon(
+                    Icons.navigate_before,
+                    color: Colors.transparent,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(24),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildShipmentNumber(),
+              _buildProductType(),
+              _buildProductName(),
+              _buildDescription(),
+              _buildChargeableWeight(),
+              _buildFees(),
+              _buildInsuranceFees(),
+              _buildAmount(),
+              SizedBox(height: 100),
+              RaisedButton(
+                child: Text(
+                  'Submit',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                onPressed: () {
+                  if (!_formKey.currentState.validate()) {
+                    return;
+                  }
+
+                  _formKey.currentState.save();
+
+                  print(productType);
+                  print(productName);
+                  print(productDescription);
+                  print(chargeableWeight);
+                  print(fees);
+                  print(insuranceFee);
+                  print(amount);
+
+                  //Send to API
+                },
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
